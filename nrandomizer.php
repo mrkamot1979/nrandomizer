61680723dce78d9bf1220213b925821a3017e82b
nino.roldan <nino_roldan@yahoo.com>
2014-07-24 17:19:25 +0900

    put in the random mechanism

--- nrandomizer.php
+++ nrandomizer.php
@@ -37,16 +37,18 @@
 		</head>
 
 	<body>
-		<?php //create a randomizer ;?>
+		<?php 
+		$randomkey = rand(0,3);
+		?>
 		
 		
 		<center>
-			<font class="big"><?php echo $jpwords[101]["jpword"];?></font>
+			<font class="big"><?php echo $jpwords[$randomkey]["jpword"];?></font>
 			<br><br>
 			<!-- making these text "invisible" by making them white -->
-			<font class="hidden" size="36"><?php echo $jpwords[101]["yomikata"];?></font>
+			<font class="hidden" size="36"><?php echo $jpwords[$randomkey]["yomikata"];?></font>
 			<br><br>
-			<font class="hidden"  size="36"><?php echo $jpwords[101]["engdef"];?></font>
+			<font class="hidden"  size="36"><?php echo $jpwords[$randomkey]["engdef"];?></font>
 		</center>
 
 	</body>
