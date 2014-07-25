cd330a8b61e882bcf38ed87559ffecaaa44368d5
nino.roldan <nino_roldan@yahoo.com>
2014-07-25 10:23:19 +0900

    first commit

---
+++ jpwords.php
@@ -0,0 +1,49 @@
+<?php
+
+header('Content-Type: text/html;charset=utf-8');
+	//create an array containing the text
+	$jpwords = array();
+	$jpwords[0] = array(
+		"jpword" => "容量",
+		"yomikata" => "ようりょう",
+		"engdef" => "capacity"
+	);
+	$jpwords[1] = array(
+		"jpword" => "消費電力",
+		"yomikata" => "しょうひでんりょく",
+		"engdef" => "electic consumption"
+	);
+	
+	$jpwords[2] = array(
+		"jpword" => "買い替えた",
+		"yomikata" => "かいかいた",
+		"engdef" => "buy to change"
+	);
+	
+	$jpwords[3] = array(
+		"jpword" => "宣伝",
+		"yomikata" => "宣伝",
+		"engdef" => "advertisement"
+	);
+	
+	$jpwords[4] = array(
+		"jpword" => "宣伝",
+		"yomikata" => "宣伝",
+		"engdef" => "advertisement"
+	);
+	
+	$jpwords[5] = array(
+		"jpword" => "ゴミ箱",
+		"yomikata" => "ごみはこ",
+		"engdef" => "trashcan"
+	);
+	
+	$jpwords[5] = array(
+		"jpword" => "各駅電車",
+		"yomikata" => "かくえきでんしゃ",
+		"engdef" => "local train/stops at every station"
+	);
+	
+	
+	
+?>
