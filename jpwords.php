78c1f5390eb2724dd56ad41f2f0d3ce7535fbce4
nino.roldan <nino_roldan@yahoo.com>
2014-07-29 17:12:54 +0900

    added jpusage attribute

--- jpwords.php
+++ jpwords.php
@@ -6,24 +6,28 @@ header('Content-Type: text/html;charset=utf-8');
 	$jpwords[0] = array(
 		"jpword" => "容量",
 		"yomikata" => "ようりょう",
-		"engdef" => "capacity"
+		"engdef" => "capacity",
+		"jpusage" => "車の容量は５人です。"
 	);
 	$jpwords[1] = array(
 		"jpword" => "消費電力",
 		"yomikata" => "しょうひでんりょく",
 		"engdef" => "electic consumption"
+		"jpusage" => "毎年の夏に消費電力はいつも高くなります。"	
 	);
 	
 	$jpwords[2] = array(
 		"jpword" => "買い替えた",
 		"yomikata" => "かいかいた",
 		"engdef" => "buy to change"
+		"jpusage" => "次のボナースは新しい車を買い替えた。"	
 	);
 	
 	$jpwords[3] = array(
 		"jpword" => "宣伝",
-		"yomikata" => "宣伝",
+		"yomikata" => "せんでん",
 		"engdef" => "advertisement"
+		"jpusage" => "宣伝によると、今年のモデルのサイズは変わりません。"
 	);
 	
 	$jpwords[4] = array(
