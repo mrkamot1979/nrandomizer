f6205faed60235ebbf084e3860343b5d23d39e29
nino.roldan <nino_roldan@yahoo.com>
2014-07-29 17:21:54 +0900

    updated

--- jpwords.php
+++ jpwords.php
@@ -12,83 +12,88 @@ header('Content-Type: text/html;charset=utf-8');
 	$jpwords[1] = array(
 		"jpword" => "消費電力",
 		"yomikata" => "しょうひでんりょく",
-		"engdef" => "electic consumption"
+		"engdef" => "electic consumption",
 		"jpusage" => "毎年の夏に消費電力はいつも高くなります。"	
 	);
 	
 	$jpwords[2] = array(
 		"jpword" => "買い替えた",
 		"yomikata" => "かいかいた",
-		"engdef" => "buy to change"
+		"engdef" => "buy to change",
 		"jpusage" => "次のボナースは新しい車を買い替えた。"	
 	);
 	
 	$jpwords[3] = array(
 		"jpword" => "宣伝",
 		"yomikata" => "せんでん",
-		"engdef" => "advertisement"
+		"engdef" => "advertisement",
 		"jpusage" => "宣伝によると、今年のモデルのサイズは変わりません。"
 	);
 	
+		
 	$jpwords[4] = array(
-		"jpword" => "宣伝",
-		"yomikata" => "宣伝",
-		"engdef" => "advertisement"
-	);
-	
-	$jpwords[5] = array(
 		"jpword" => "ゴミ箱",
 		"yomikata" => "ごみはこ",
-		"engdef" => "trashcan"
+		"engdef" => "trashcan",
+		"jpusage" => "この公園であんまりゴミ箱がないですようね。";	
 	);
 	
 	$jpwords[5] = array(
 		"jpword" => "各駅電車",
 		"yomikata" => "かくえきでんしゃ",
 		"engdef" => "local train/stops at every station"
+		"jpusage" => "次の駅は各駅電車と横浜で止ります。"
+
 	);
 	
 	$jpwords[6] = array(
 		"jpword" => "快速",
 		"yomikata" => "かいそく",
-		"engdef" => "high speed i.e. train"
+		"engdef" => "high speed i.e. train",
+		"jpusage" => "#"
 	);
 	
 	$jpwords[7] = array(
 		"jpword" => "歩行者",
 		"yomikata" => "ほこうしゃ",
-		"engdef" => "pedestrian"
+		"engdef" => "pedestrian",
+		"jpusage" => "#"
 	);
 	
 	$jpwords[8] = array(
 		"jpword" => "歩行者",
 		"yomikata" => "ほこうしゃ",
-		"engdef" => "pedestrian"
+		"engdef" => "pedestrian",
+		"jpusage" => "#"
 	);
 	
 	$jpwords[9] = array(
 		"jpword" => "特徴",
 		"yomikata" => "とくちょう",
-		"engdef" => "characteristics/functions"
+		"engdef" => "characteristics/functions",
+		"jpusage" => "#"
 	);
 	
 	$jpwords[10] = array(
 		"jpword" => "友人",
 		"yomikata" => "ゆうじん",
-		"engdef" => "friend"
+		"engdef" => "friend",
+		"jpusage" => "#"
 	);
 	
 $jpwords[11] = array(
 		"jpword" => "整理",
 		"yomikata" => "せいり",
-		"engdef" => "to arrange"
+		"engdef" => "to arrange",
+		"jpusage" => "#"
 	);
 	
-		
+
 $jpwords[12] = array(
 		"jpword" => "少",
 		"yomikata" => "すく",
-		"engdef" => "a little, a few e.g. 少ない時間 means 'limited time'"
+		"engdef" => "a little, a few e.g. 少ない時間 means 'limited time'",
+		"jpusage" => "#"
 	);
 	
 	
