b3b3f50a7913c51c4d19ecb9d3c76fd73aca1a49
nino.roldan <nino_roldan@yahoo.com>
2014-08-08 17:30:14 +0900

    created function to satisfy SoC requirements

--- nrandomizer.php
+++ nrandomizer.php
@@ -9,29 +9,35 @@
 		<head>
 			<style>
 			.big { font-size: 520%; color : red;}	
-			.hidden { color: white;}
+			.hidden { color: white; font-size: 36;}
 			</style>
 		</head>
 
 	<body>
-		<?php 
-		$randomword = getRandomJPWord();
-		echo $randomword;
-		exit;
-		?>
-		
-		
 		<center>
-			<?php $jpwords = getAllJPWords(); ?>
+		<?php $randomword = getRandomJPWord(); ?>
+		<font class="big"><?php echo $randomword[0]; ?></font>
+		<br><br>
+		<font class="hidden"><?php echo $randomword[1]; ?></font>
+		<br><br>
+		<font class="hidden"><?php echo $randomword[2]; ?></font>
+		<br><br>
+		<font class="hidden"><?php echo $randomword[3]; ?> </font>
+		</center>
+	<!--	
+		<center>
+			
 			<font class="big"><?php echo $jpwords[$randomkey]["jpword"];?></font>
 			<br><br>
-			<!-- making these text "invisible" by making them white -->
+			<making these text "invisible" by making them white >
 			<font class="hidden" size="36"><?php echo $jpwords[$randomkey]["yomikata"];?></font>
 			<br><br>
 			<font class="hidden"  size="36"><?php echo $jpwords[$randomkey]["engdef"];?></font>
 			<br><br>
 			<font class="hidden" size="36"><?php echo $jpwords[$randomkey]["jpusage"]?> </font>
-		</center>
+		</center-->
+
+	
 
 	</body>
 </html>
